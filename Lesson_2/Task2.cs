d2f8e5cee7569c72c383840de1fc6dec171b108d
YuriGaluza <74955101+YuriGaluza@users.noreply.github.com>
2021-02-18 00:33:24 +0600

    Add files via upload

--- Lesson_2/Task2.cs
+++ Lesson_2/Task2.cs
@@ -6,16 +6,15 @@ namespace Level_1
 {
     class Task2
     {
+        //1. Запросить у пользователя минимальную и максимальную температуру за сутки
+        //и вывести среднесуточную температуру
         public double AverageDailyTemperature()
         {
-            //1.	Запросить у пользователя минимальную и максимальную температуру за сутки
-            //и вывести среднесуточную температуру
             Console.Write("Введите минимальную суточную температуру: ");
             int minTemperature = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите максимальную суточную температуру: ");
             int maxTemperature = Convert.ToInt32(Console.ReadLine());
             double averageTemperature = 0.5 * (minTemperature + maxTemperature);
-
 
             if (maxTemperature > minTemperature)
             {
@@ -23,18 +22,19 @@ namespace Level_1
             }
             else if (maxTemperature < minTemperature)
             {
-                Console.WriteLine($"Минимальная температура должна быть иеньше максимальной ");
+                Console.WriteLine($"Минимальная температура должна быть меньше максимальной ");
             }
+            Console.WriteLine();
             return averageTemperature;
         }
 
-        //2.	Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+        //2. Запросить у пользователя порядковый номер текущего месяца и вывести его название.
         public int GetMonthByNumber()
         {
             Console.Write("Введите порядковый номер месяца: ");
             int numberOfMonth = Convert.ToInt32(Console.ReadLine());
 
-            if (numberOfMonth > 12 && numberOfMonth < 0)
+            if (numberOfMonth > 12 || numberOfMonth < 0)
             {
                 Console.WriteLine($"Номер месяц должен быть больше 0 и меньше 13");
             }
@@ -42,10 +42,11 @@ namespace Level_1
             {
                 Console.WriteLine($"Месяц по номеру {numberOfMonth} является " + (Months)numberOfMonth);
             }
+            Console.WriteLine();
             return numberOfMonth;
         }
 
-        //3.	Определить, является ли введённое пользователем число чётным.
+        //3. Определить, является ли введённое пользователем число чётным.
         public void IsEven()
         {
             Console.Write("Введите любое натуральное число: ");
@@ -58,9 +59,32 @@ namespace Level_1
             {
                 Console.WriteLine($"Число {value} нечётное");
             }
+            Console.WriteLine();
         }
 
-        //4.	(*) Если пользователь указал месяц из зимнего периода,
+        //4. Для полного закрепления понимания простых типов найдите любой чек,
+        //либо фотографию этого чека в интернете и схематично нарисуйте его в консоли,
+        //только за место динамических, по вашему мнению,
+        //данных (это может быть дата, название магазина, сумма покупок) подставляйте переменные,
+        //которые были заранее заготовлены до вывода на консоль.
+        public void ShowPayCheck()
+        {
+            int payCheckNumber = 1245565;
+            string product1_name = "Сахар песок фас. 1кг";
+            double product1_cost = 85.36;
+            int product1_quantity = 3;
+            string product2_name = "ГОСТ Молоко питьевое паст";
+            double product2_cost = 35.34;
+            int product2_quantity = 2;
+
+            string message = $"Магнит\nЗАО Тандер\nЧек №{payCheckNumber} \n" +
+                $"ЧЕК НА ПРОДАЖУ \n1.{product1_name} *{product1_quantity}={product1_cost}\n" +
+                $"2.{product2_name} *{product2_quantity}={product2_cost}\n" +
+                $"ИТОГО БЕЗ СКИДКИ = {product1_quantity * product1_cost + product2_quantity + product2_quantity * product2_cost}";
+            Console.WriteLine(message);
+        }
+
+        //5. (*) Если пользователь указал месяц из зимнего периода,
         //а средняя температура > 0, вывести сообщение «Дождливая зима».
         public void RainyWinter(double averageTemperature, int numberOfMonth)
         {
@@ -70,9 +94,10 @@ namespace Level_1
             {
                 Console.WriteLine($"Месяц {(Months)numberOfMonth}, среднесуточная температура {averageTemperature}");
             }
+            Console.WriteLine();
         }
         
-
+        [Flags]
         public enum Months
         {
             January = 1,
--- Program.cs
+++ Program.cs
@@ -6,14 +6,21 @@ namespace Level_1
     {
         static void Main(string[] args)
         {
-            Task1 Task1 = new Task1();
-            Task1.HelloUser();
+            //Task1 Task1 = new Task1();
+            //Task1.HelloUser();
 
-            Task2 Task2 = new Task2();
-            double averageDailyTemperature = Task2.AverageDailyTemperature();
-            int monthByNumber = Task2.GetMonthByNumber();
-            Task2.IsEven();
-            Task2.RainyWinter(averageDailyTemperature, monthByNumber);
+            //Task2 Task2 = new Task2();
+            //double averageDailyTemperature = Task2.AverageDailyTemperature();
+            //int monthByNumber = Task2.GetMonthByNumber();
+            //Task2.IsEven();
+            //Task2.ShowPayCheck();
+            //Task2.RainyWinter(averageDailyTemperature, monthByNumber);
+
+            Task3 task3 = new Task3();
+            task3.DiagonalElementsOfArray(new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+            task3.GetPhoneBook();
+            task3.ReverseString("Hello");
+            task3.BattleShip();
         }
     }
 }
